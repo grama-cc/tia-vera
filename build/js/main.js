8478f32eaab5a8c531e53d2fcb6bcd6b7c3ee626
Harrison Mendonça <h@harrison.com.br>
2016-02-02 22:39:37 -0200

    add builded files

---
+++ build/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function () {
+
+  var form = $('#form-checkout');
+
+  form.submit(function () {
+    PagSeguroLightbox(this);
+    return false;
+  });
+
+  $('.product').each(function () {
+    var product = $(this);
+    var code = product.attr('code');
+    console.log(code);
+
+    $('button', product).click(function () {
+      $('[name=itemCode]').val(code);
+      form.submit();
+    });
+  });
+})();
+//# sourceMappingURL=main.js.map
